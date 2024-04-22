97562aa7135e9ec3ef73b9690f4d239f836e6ed1
YelliottTheFringe <123002260+YelliottTheFringe@users.noreply.github.com>
2024-04-22 06:30:12 -0500

    Add files via upload

--- Network-Game-Tutorial-master/Custom_GameC.py
+++ Network-Game-Tutorial-master/Custom_GameC.py
@@ -1,6 +1,8 @@
 import socket as sk
 from time import sleep
-coord=(12,50)
+import pygame as p
+import dataParser as pr
+
 def waitfor(signal):
     brk=True
     while brk:
@@ -9,19 +11,43 @@ def waitfor(signal):
             break
         else:
             sleep(0.001)
-def move():
+
     pass
-def dataParserR(data): #cut the data recieved from the server into the two coordinates. (recv)
-    pass
-def dataParserS(data): #(send)
-    message=''
-    message+=str(data[0])+"n"+str(data[1])
-    return message
+p.init()
+sh=600
+sw=500
+window=p.display.set_mode((sh,sw))
+p.display.set_caption("Pong")
 sock=sk.socket(sk.AF_INET,sk.SOCK_STREAM)
-sock.connect(('127.0.0.1',12002))
+sock.connect(('192.168.9.247',12002))
 waitfor('000')
+clientNum=sock.recv(1024).decode()
+if clientNum=='A':
+    c=60
+elif clientNum=='B':
+    c=sh-70
+v=100
 game=True
 while game:
-    print((dataParserS(coord)))
-    sock.send(dataParserS(coord).encode())
-    sock.recv(1024)
+    coord=(c,v)
+    sock.send(pr.parseOutSingle(coord))
+    window.fill('Black')
+    A,B=pr.parseInDouble(sock.recv(1024))
+    p.draw.rect(window,'Red',(B[0],B[1],20,60)) #oponent
+    p.draw.circle(window,"White",A,15)
+    p.draw.rect(window,'White',(coord[0],coord[1],20,60)) # self
+    keys = p.key.get_pressed()
+    for event in p.event.get():
+        if event.type == p.QUIT:
+            p.quit()
+    if keys[p.K_w] and v>3:
+        v-=1
+    elif keys[p.K_s]and v<(sw-60):
+        v+=1
+    if v<=3:
+        v+=1
+    elif v>(sw-30):
+        v-=1
+    p.display.update()
+    pr.c(coord)
+    
--- Network-Game-Tutorial-master/Custom_GameS.py
+++ Network-Game-Tutorial-master/Custom_GameS.py
@@ -1,33 +1,58 @@
 import socket as sk
 import pygame as p
-def dataParserR(data): #Parse the received data into a tuple for the server in watch mode
-    pass
-def dataParserS(data): #Parse the outgoing data, both cordinates sandwiched together in strings.
-    print(data)
-
-    
-
-sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
-sock.bind(('',12002))
-sock.listen(2)
-game=True
-con=[]
-coordC=(0,0)
-cliSockA, cliIPA = sock.accept()
-print("Player 1 connected!")
-cliSockB, cliIPB = sock.accept()
-print("Player 2 connected! Game begins.")
-cliSockA.send('000'.encode())
-cliSockB.send('000'.encode())
+import dataParser as pr  
+from random import randint
+import math as m 
+def initialize():
+    global sock,game,con,cC,cliSockA,cliSockB, sh,sw, x,y,angle,mt
+    sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+    sock.bind(('',12002))
+    sh=600
+    sw=500 
+    mt=0.08
+    angle = 200
+    x = 300
+    y = 250
+    sock.listen(2)
+    game=True
+    con=[]
+    cC=(300,250)
+    pr.c("Line 20")
+    cliSockA, cliIPA = sock.accept()
+    print("Player 1 connected!")
+    cliSockB, cliIPB = sock.accept()
+    print("Player 2 connected! Game begins.")
+    cliSockA.send('000'.encode())
+    cliSockA.send('A'.encode())
+    cliSockB.send('000'.encode())
+    cliSockB.send('B'.encode())
+initialize()
+pr.c("Initialized and ready.")
 while game:
-    coordA = (cliSockA.recv(1024).decode())
-    coordB = (cliSockB.recv(1024).decode())
-    print(coordA)
-    print(coordB)
-    outCoordsA=coordC,coordB
-    outCoordsB=coordC,coordA
-    outCoordsA=dataParserS(outCoordsA)
-    width=20
-    height=60
-    sock.close()
-    break
+    cA = pr.parseInSingle(cliSockA.recv(1024))
+    conditA=(cC[0]+20>=cA[0]-20 and cC[0]+20<=cA[0]+20 and cC[1]+20>=cA[1]-60 and cC[1]+20<=cA[1]+60 and cC[1]-20>=cA[1]-60 and cC[1]-20<=cA[1]+60)
+    cB = pr.parseInSingle(cliSockB.recv(1024))
+    conditB=(cC[0]-20>=cB[0]-20 and cC[0]-20<=cB[0]+20 and cC[1]+20>=cB[1]-60 and cC[1]+20<=cB[1]+60 and cC[1]-20>=cB[1]-60 and cC[1]-20<=cB[1]+60)
+    x  = x+int((mt*m.degrees(m.cos(m.radians(angle/15)))))
+    y  = y+int((mt*m.degrees(m.sin(m.radians(angle/15)))))
+    pr.c(int((mt*m.degrees(m.cos(m.radians(angle))))/10)+1)
+    if x<=20 or x >580 or y<=20 or y>480 or conditA or conditB:
+        mt=-mt
+        angle = randint(1,360)
+        if x<=20:
+            x=21
+        if x>=580:
+            x=579
+        if y<=20:
+            y=21
+        if y>=480:
+            y=479
+    if angle>360:
+        angle -=360
+    elif angle<0:
+        angle +=360
+    cC=(x,y)
+    outCoordsA=cC,cB
+    outCoordsB=cC,cA
+    cliSockA.send(pr.parseOutDouble(outCoordsA))
+    cliSockB.send(pr.parseOutDouble(outCoordsB))
