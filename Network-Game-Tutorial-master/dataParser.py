e27dbf1704e66c107d79c45900a6ea7a7754e77f
YelliottTheFringe <123002260+YelliottTheFringe@users.noreply.github.com>
2024-04-24 06:28:50 -0500

    Add files via upload

--- Network-Game-Tutorial-master/dataParser.py
+++ Network-Game-Tutorial-master/dataParser.py
@@ -1,3 +1,42 @@
-
-coordC=(0,0)
-outCoordsA=coordC,coordB
+import sys as s
+def c (data):
+    s.stdout.write(str(data))
+    s.stdout.flush()
+def parseOutDouble(coords):
+    outString=''
+    for i in range(0,2):
+        for v in range(0,2):
+            outString+=str(coords[i][v])
+            outString+='-'
+    return(outString.encode())
+def parseInDouble(data):
+    coords=[]
+    num_holder=''
+    for i in data.decode():
+        if i=="-":
+            coords.append(num_holder)
+            num_holder=''
+        else:
+            num_holder+=i
+    coords.append(num_holder)
+    coordA=(int(coords[0]),int(coords[1]))
+    coordD=(int(coords[2]),int(coords[3]))
+    return coordA,coordD
+def parseOutSingle(data):
+        outString=''
+        for v in range(0,2):
+            outString+=str(data[v])
+            outString+='-'
+        return(outString.encode())
+def parseInSingle(data):
+    coords=[]
+    num_holder=''
+    for i in data.decode():
+        if i=="-":
+            coords.append(num_holder)
+            num_holder=''
+        else:
+            num_holder+=i
+    coords.append(num_holder)
+    coordA=(int(coords[0]),int(coords[1]))
+    return coordA
